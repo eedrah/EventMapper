55a2872b5efcc2205b674ac258ff84c5a7ec987b
Eedrah <eedrah@users.noreply.github.com>
2015-03-03 10:55:10 +1300

    update nugets

--- EventMapper.Tests/Controllers/HomeControllerTest.cs
+++ EventMapper.Tests/Controllers/HomeControllerTest.cs
@@ -19,7 +19,6 @@ namespace EventMapper.Tests.Controllers
 
             // Assert
             Assert.IsNotNull(result);
-            Assert.AreEqual("Home Page", result.ViewBag.Title);
         }
     }
 }
