25d5197d5bde5555744b0ddfa167541eae1c1c71
Mitchell-McEwen <mitchell.mcewen@users.noreply.github.com>
2015-03-02 16:10:28 +1300

    added initial solution

---
+++ EventMapper/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EventMapper.WebApiApplication" Language="C#" %>
---
+++ EventMapper/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(EventMapper.Startup))]
+
+namespace EventMapper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
