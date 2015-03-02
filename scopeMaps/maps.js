6da0b9506c1fcf3a7461c78b9365472b1ef7afca
Eedrah <eedrah@users.noreply.github.com>
2015-03-02 20:31:29 +1300

    demo map markers and infoWindows

--- scopeMaps/maps.js
+++ scopeMaps/maps.js
@@ -9,4 +9,18 @@ function initialize() {
         document.getElementById('map-canvas'),
         mapOptions
     );
-}
+
+    var marker = new google.maps.Marker({
+        position: map.getCenter(),
+        map: map, // or marker.setMap(map);
+        title: 'This is my hover text'
+    });
+
+    var infoWindow = new google.maps.InfoWindow();
+    infoWindow.setContent("<p>Surely there is a better way to pass html... jQuery?</p>");
+    infoWindow.setPosition(marker.getPosition());
+
+    google.maps.event.addListener(marker, 'click', function(){
+        infoWindow.open(map);
+    });
+};
