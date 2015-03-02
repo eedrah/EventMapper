3c93e8203e165435970490602bac566fc3e206aa
Eedrah <eedrah@users.noreply.github.com>
2015-03-03 11:08:33 +1300

    set up base tests

---
+++ EventMapper.Tests/Controllers/SearchControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EventMapper;
+using EventMapper.Controllers;
+
+namespace EventMapper.Tests.Controllers
+{
+    [TestClass]
+    public class SearchControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            SearchController controller = new SearchController();
+
+            // Act
+            IEnumerable<string> result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("value1", result.ElementAt(0));
+            Assert.AreEqual("value2", result.ElementAt(1));
+        }
+    }
+}
---
+++ EventMapper/Controllers/SearchController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EventMapper.Controllers
+{
+    public class SearchController : ApiController
+    {
+        // GET api/search
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
