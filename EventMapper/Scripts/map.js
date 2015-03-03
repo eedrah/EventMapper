8945e1b4cc214eb83e2b3b385bfdb94f03bdfa47
Mitchell-McEwen <mitchell.mcewen@users.noreply.github.com>
2015-03-03 19:57:25 +1300

    removed commented out code

--- EventMapper/Scripts/map.js
+++ EventMapper/Scripts/map.js
@@ -1,24 +1,8 @@
 ﻿var map;
 
-//var json_array = JSON.parse(json_string);
-
-function Event (title, start, end, lng, lat, url) {
-    this.title = title;
-    this.start = start;
-    this.end = end;
-    this.lng = lng;
-    this.lat = lat;
-    this.url = url;
-}
+var json_array = JSON.parse(json_string);
 
 var markers = [];
-
-var eventsArray = [
-['Wellington', -41.294, 174.7753],
-['wellington chamber orchestra', -41.2801, 174.7753]
-];
-
-eventObjects(eventsArray);
 
 function eventObjects(array) {
     for (var i = 0; i < array.length; i++) {
@@ -27,6 +11,7 @@ function eventObjects(array) {
     }
 }
 
+eventObjects(json_array);
 
 function initialize() {
     var mapOptions = {
@@ -35,12 +20,6 @@ function initialize() {
     };
 
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-    
-    //var markers = [
-    //['Wellington Raw Comedy Quest', -41.294, 174.7753],
-    //['WIT Worldwide Wellington Tour', -41.2179, 175.4596],
-    //['Wellington Chamber Orchestra', -41.2801, 174.7753]
-    //];
 
     var marker, i;
 
