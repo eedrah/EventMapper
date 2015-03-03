3f9047d0e975d19c2951699763e1b34250a60f76
Eedrah <eedrah@users.noreply.github.com>
2015-03-03 17:00:04 +1300

    add simple injector

---
+++ EventMapper/App_Start/SimpleInjectorWebApiInitializer.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using EventMapper.Models;
+using EventMapper.Models.Interfaces;
+
+[assembly: WebActivator.PostApplicationStartMethod(typeof(EventMapper.App_Start.SimpleInjectorWebApiInitializer), "Initialize")]
+
+namespace EventMapper.App_Start
+{
+    using System.Web.Http;
+    using SimpleInjector;
+    using SimpleInjector.Integration.WebApi;
+    
+    public static class SimpleInjectorWebApiInitializer
+    {
+        /// <summary>Initialize the container and register it as MVC3 Dependency Resolver.</summary>
+        public static void Initialize()
+        {
+            // Did you know the container can diagnose your configuration? Go to: https://bit.ly/YE8OJj.
+            var container = new Container();
+            
+            InitializeContainer(container);
+
+            container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
+       
+            container.Verify();
+            
+            GlobalConfiguration.Configuration.DependencyResolver =
+                new SimpleInjectorWebApiDependencyResolver(container);
+        }
+     
+        private static void InitializeContainer(Container container)
+        {
+            container.RegisterWebApiRequest<IEventFinder, EventFinder>();
+        }
+    }
+}
