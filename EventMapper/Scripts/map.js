7fd9c8dbc57721097cbe45ebe7732ed3fe88b809
Mitchell-McEwen <mitchell.mcewen@users.noreply.github.com>
2015-03-03 15:01:58 +1300

    render map on page

--- EventMapper/Scripts/map.js
+++ EventMapper/Scripts/map.js
@@ -1,12 +1,13 @@
 ﻿var map;
 function initialize() {
     var mapOptions = {
-        zoom: 10,
+        zoom: 13,
         center: new google.maps.LatLng(-41.2958703, 174.7981961)
     };
 
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
