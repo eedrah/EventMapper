43cf26d628ab7df899f5e9d2664f92363fbd54bb
Eedrah <eedrah@users.noreply.github.com>
2015-03-03 18:24:53 +1300

    start eventfinder requests

--- EventMapper/Models/EventFinder.cs
+++ EventMapper/Models/EventFinder.cs
@@ -2,12 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventMapper.Controllers;
 using EventMapper.Models.Interfaces;
+using RestSharp;
 
 namespace EventMapper.Models
 {
     public class EventFinder : IEventFinder
     {
+        private const string ApiDomain = "http://api.eventfinder.co.nz";
+        private const string ApiResource = "v2/events.xml";
+
+        private static readonly RestClient EventFinderClient = new RestClient(ApiDomain);
+
+        public EventFinder()
+        {
+            EventFinderClient.Authenticator = new SimpleAuthenticator("username", "workingtitlewhatshappening", "password", "7xff9nnttb94");
+        }
+
         public IEnumerable<EventItem> Search(string searchTerm)
         {
             return new List<EventItem>
@@ -24,6 +36,12 @@ namespace EventMapper.Models
                     Price = 12.99m
                 }
             };
+        }
+
+        private string MakeEventFinderRequest()
+        {
+            RestRequest request = new RestRequest(ApiResource, Method.GET);            
+            request.AddQueryParameter("row", "10")
         }
     }
 }
