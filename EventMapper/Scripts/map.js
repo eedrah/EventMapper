109e9aa5ef04dce22a5ba00ec43562949cc07b3c
Mitchell-McEwen <mitchell.mcewen@users.noreply.github.com>
2015-03-04 09:44:57 +1300

    moved marker code from map.js to markers.js

--- EventMapper/Scripts/map.js
+++ EventMapper/Scripts/map.js
@@ -1,37 +1,10 @@
-﻿//var json_array = JSON.parse(json_string);
-
-//var markers = [];
-
-//function eventObjects(array) {
-//    for (var i = 0; i < array.length; i++) {
-//        markers.push(array[i]);
-
-//    }
-//}
-
-var markers = [
-    ["Title", -41, 175],
-];
-//eventObjects(json_array);
-
-function initialize() {
+﻿function initialize() {
     var mapOptions = {
         zoom: 9,
         center: new google.maps.LatLng(-41.1475787, 175.540889)
     };
 
    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-    var marker, i;
-
-    for (i = 0; i < markers.length; i++) {
-      marker = new google.maps.Marker({
-            position: new google.maps.LatLng(markers[i][0],markers[i][1]),
-            map: map,
-            title: markers[i][2]
-        });
-
-    }
     
 }
 
--- EventMapper/Scripts/markers.js
+++ EventMapper/Scripts/markers.js
@@ -1,1 +1,30 @@
 ﻿
+
+
+
+//var json_array = JSON.parse(json_string);
+
+//var markers = [];
+
+//function eventObjects(array) {
+//    for (var i = 0; i < array.length; i++) {
+//        markers.push(array[i]);
+
+//    }
+//}
+
+var markers = [
+    ["Title", -41, 175],
+];
+//eventObjects(json_array);
+
+var marker, i;
+
+for (i = 0; i < markers.length; i++) {
+    marker = new google.maps.Marker({
+        position: new google.maps.LatLng(markers[i][0], markers[i][1]),
+        map: map,
+        title: markers[i][2]
+    });
+
+}
