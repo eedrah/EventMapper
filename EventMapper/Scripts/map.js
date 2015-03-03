6da59d218a64fc8a97dc115de67c2cbe778cdb17
Mitchell-McEwen <mitchell.mcewen@users.noreply.github.com>
2015-03-03 17:00:36 +1300

    started marker code

--- EventMapper/Scripts/map.js
+++ EventMapper/Scripts/map.js
@@ -7,7 +7,25 @@ function initialize() {
 
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+    var markers = [
+    ['Wellington Raw Comedy Quest', -41.294, 174.7753]
+    ['WIT\'s Worldwide Wellington Tour', -41.2179, 175.4596]
+    ['Wellington Chamber Orchestra', -41.2801, 174.7753]
+    ];
 
+
+    for (var i = 0; i < markers.Length; i++) {
+        var position = new google.maps.Latlng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        markers = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0]
+        });
+
+    }
+
+    }
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
