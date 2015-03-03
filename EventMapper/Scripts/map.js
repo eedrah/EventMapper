4b03caa4ce19195cb354ed9a80f4eb151f4377e4
Mitchell-McEwen <mitchell.mcewen@users.noreply.github.com>
2015-03-03 19:54:34 +1300

    code for parsing json string to markers array

--- EventMapper/Scripts/map.js
+++ EventMapper/Scripts/map.js
@@ -1,4 +1,33 @@
 ﻿var map;
+
+//var json_array = JSON.parse(json_string);
+
+function Event (title, start, end, lng, lat, url) {
+    this.title = title;
+    this.start = start;
+    this.end = end;
+    this.lng = lng;
+    this.lat = lat;
+    this.url = url;
+}
+
+var markers = [];
+
+var eventsArray = [
+['Wellington', -41.294, 174.7753],
+['wellington chamber orchestra', -41.2801, 174.7753]
+];
+
+eventObjects(eventsArray);
+
+function eventObjects(array) {
+    for (var i = 0; i < array.length; i++) {
+        markers.push(array[i]);
+
+    }
+}
+
+
 function initialize() {
     var mapOptions = {
         zoom: 9,
@@ -6,26 +35,25 @@ function initialize() {
     };
 
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    
+    //var markers = [
+    //['Wellington Raw Comedy Quest', -41.294, 174.7753],
+    //['WIT Worldwide Wellington Tour', -41.2179, 175.4596],
+    //['Wellington Chamber Orchestra', -41.2801, 174.7753]
+    //];
 
-    var markers = [
-    ['Wellington Raw Comedy Quest', -41.294, 174.7753]
-    ['WIT\'s Worldwide Wellington Tour', -41.2179, 175.4596]
-    ['Wellington Chamber Orchestra', -41.2801, 174.7753]
-    ];
+    var marker, i;
 
-
-    for (var i = 0; i < markers.Length; i++) {
-        var position = new google.maps.Latlng(markers[i][1], markers[i][2]);
-        bounds.extend(position);
-        markers = new google.maps.Marker({
-            position: position,
+    for (i = 0; i < markers.length; i++) {
+      marker = new google.maps.Marker({
+            position: new google.maps.LatLng(markers[i][1],markers[i][2]),
             map: map,
             title: markers[i][0]
         });
 
     }
-
-    }
+    
+}
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
