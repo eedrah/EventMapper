d2a2e9395183a1da9dcda2c0e0a39e6456ff0e08
Mitchell-McEwen <mitchell.mcewen@users.noreply.github.com>
2015-03-04 16:43:53 +1300

    add trimString method

--- EventMapper/Models/EventItem.cs
+++ EventMapper/Models/EventItem.cs
@@ -6,13 +6,21 @@ using System.Xml.Serialization;
 using RestSharp.Deserializers;
 
 namespace EventMapper.Models {
-    public class EventItem {
-        [XmlElement("name")] public string Title { get; set; }
+    public class EventItem
+    {
+        private string _title;
+        [XmlElement("name")] public string Title
+        {
+            get { return _title; } 
+
+            set {_title = TrimString(value); }
+        }
+
         [XmlElement("description")] public string Description { get; set; }
-        //[XmlElement("datetime_start")] public DateTime Start { get; set; }
-        //[XmlElement("datetime_end")] public DateTime End { get; set; }
+        [XmlElement("datetime_start")] public string Start { get; set; }
+        [XmlElement("datetime_end")] public string End { get; set; }
         //public decimal Price { get; set; }
-        //[XmlElement("url")] public Uri Link { get; set; }
+        [XmlElement("url")] public string Link { get; set; }
         [XmlElement("location_summary")] public string LocationSummary { get; set; }
         //[XmlElement("is_free")] public bool IsFree { get; set; }
 
@@ -21,6 +29,16 @@ namespace EventMapper.Models {
 
         //public double Latitude { get { return Point.lat; } }
         //public double Longitude { get { return Point.lng; } }
+
+        public string TrimString(string s)
+        {
+            if (s.Contains("<!"))
+            {
+               s = s.Remove(0, 9);
+            }
+            return s;
+        }
+    
     }
 
     public class Point
