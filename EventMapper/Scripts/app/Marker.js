f38b5e9e5b8566dfd052fa224a8ec9d0b8ee9fde
Eedrah <eedrah@users.noreply.github.com>
2015-03-05 11:57:18 +1300

    start creating data for Marker

--- EventMapper/Scripts/app/Marker.js
+++ EventMapper/Scripts/app/Marker.js
@@ -1,5 +1,22 @@
 ﻿function Marker() {
-    this.parseJson = function(eventJson) {
+    //this.position
+    //this.infoWindow
+
+    this.parseJson = function(json) {
+        this.position = { lat: json.Point.lat, lng: json.Point.lng };
+
+        var $link = $('<a>').text("More details").attr('href', json.Link);
+        var $description = $('<p>').text(json.Description);
+        var $title = $('<h5>').text(json.Title);
+        var $locationSummary = $('<p>').text(json.LocationSummary)
+
+        var $windowBody = $('<div>').addClass('eventPopup');
+        $windowBody.append($title)
+            .append($description)
+            .append($locationSummary)
+            .append($link);
+
         this.infoWindow = new google.maps.InfoWindow({ maxWidth: 200 });
+        this.infoWindow.setContent($windowBody[0]);
     };
 };
