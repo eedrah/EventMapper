a57711c825b3900d5c513bcdc85cd2db5af77403
Eedrah <eedrah@users.noreply.github.com>
2015-03-06 11:22:47 +1300

    create example tests

---
+++ EventMapper.Tests/Scripts/exampleTest.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="~/Scripts/jasmine.js" />
+/// <reference path="~/../EventMapper/Scripts/app/MapController.js" />
+
+describe("Jasmine works", function () {
+    //var restService = null;
+    //beforeEach(function () {
+    //    restService = 5;
+    //});
+    //afterEach(function () {
+    //    restService = null;
+    //});
+    it("can run a test", function () {
+        expect(4).toBe(4);
+    });
+});
+
+describe("MapController", function() {
+    it('can create a mapController', function() {
+        var mc = new MapController();
+        expect(mc).not().toBeNull();
+    });
+});
